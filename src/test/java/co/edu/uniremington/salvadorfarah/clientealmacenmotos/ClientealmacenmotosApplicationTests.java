77640800fa5ea289ad96e052cf051cb7cca5c00e
EsmeRamos <esmeralda.ramos.0957@miremington.edu.co>
2026-02-05 21:04:17 -0500

    "estructura-proyecto"

---
+++ src/test/java/co/edu/uniremington/salvadorfarah/clientealmacenmotos/ClientealmacenmotosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniremington.salvadorfarah.clientealmacenmotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientealmacenmotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
